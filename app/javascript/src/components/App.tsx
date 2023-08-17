f29784091aa4dbd7b1990e1a4a8ed4bc373ee791
Akhil G Krishnan <akhilgkrishnan4u@gmail.com>
2023-08-17 20:44:29 +0530

    Updated the welcome page

--- app/javascript/src/components/App.tsx
+++ app/javascript/src/components/App.tsx
@@ -1,5 +1,19 @@
 import React from "react";
 
-const App = () => <div className="text-red-800">Welcome to ReactRailify</div>;
+const App = () => (
+  <div className="welcome-container">
+    <header className="welcome-header">
+      <h1>Welcome to ReactifyRails!</h1>
+      <p>A Rails 7.0.7 and Ruby 3.2.2 Boilerplate with React</p>
+    </header>
+    <main className="welcome-main">
+      <p>This is the welcome index page of your ReactifyRails application.</p>
+      <p>You can start building your awesome React app here.</p>
+    </main>
+    <footer className="welcome-footer">
+      <p>Created with ❤️ by Akhil G Krishnan</p>
+    </footer>
+  </div>
+);
 
 export default App;
